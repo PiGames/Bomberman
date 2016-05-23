e05764953154b1b0cd578a9454b25f75f46a02ac
Kamilosxp <rudekkamil@gmail.com>
2016-05-23 21:09:14 +0200

    Animator Update

--- Bomberman/Animator.cpp
+++ Bomberman/Animator.cpp
@@ -1,42 +1,43 @@
 #include "Animator.h"
-​
-​
-​
+
+
+
 Animator::Animator()
 {
 }
-​
-​
+
+
 Animator::~Animator()
 {
 }
-​
-​
+
+
 void Animator::SetSprite(sf::Sprite & sprite)
 {
 	sprite = m_sprite;
 }
-​
+
 bool Animator::AddAnimationState(std::string name, const TextureAtlas & atlas, const size_t & begin, const size_t & end)
-{ // TODO powinieneś dodać stan do mapy, a nie tworzyć tylko 1
-	if (!name.empty())
+{
+	it = m_states.find(name);
+	if ((!name.empty()) && !(it!=m_states.end()))
 	{
-		m_states.insert[name];
+		m_states.insert(std::pair <std::string, AnimationStateInfo>(name, AnimationStateInfo()));
 		TextureAtlas m_atlas = atlas;
-		m_states[name]->atlas = &m_atlas;
-		m_states[name]->beg = begin;
-		m_states[name]->end = end;
-​
+		m_states[name].atlas = &m_atlas;
+		m_states[name].beg = begin;
+		m_states[name].end = end;
+
 		return true;
 	}
 	return false;
 }
-​
-​
+
+
 bool Animator::ChangeActiveState(const std::string & name)
 {
-	auto search = m_states.find(name); // TODO antypattern!
-	if (search != m_states.end())
+	it = m_states.find(name);
+	if (it != m_states.end())
 	{
 		m_currentstate = name;
 		return true;
@@ -44,56 +45,58 @@ bool Animator::ChangeActiveState(const std::string & name)
 	else
 		return false;
 }
-​
-​
+
+
 std::string Animator::GetActiveState() const
 {
 	return m_currentstate;
 }
-​
-​
+
+
 void Animator::SetAnimationSpeed(const float & speed)
 {
+	m_animationspeed = speed;
 	m_delay = 10.f;
 	m_frames = speed * m_delay;
 }
-​
-​
+
+
 void Animator::SetDelayBetweenFrames(const float & delay)
 {
 	m_delay = delay;
 }
-​
-​
+
+
 void Animator::SetLoop(bool loop)
 {
 	m_loop = loop;
 }
-​
-​
+
+
 void Animator::Animate(const float & dt)
-{ // TODO kamilos fixup needed
-	/*if (dt >= m_delay)
+{
+	int m_currentFrame = m_states[m_currentstate].beg; //first frame
+	int end = m_states[m_currentstate].end; //last frame
+
+	if (!m_animIsPlaying)
+		return;
+
+	m_elapsedTime += dt;
+
+	float m_speed = 1 / m_animationspeed;
+
+	if (m_elapsedTime > m_delay* m_speed)
 	{
-​
-		size_t begin = m_states[m_currentstate]->beg;
-		size_t end = m_states[m_currentstate]->end;
-		if (m_loop)
+		m_atlas.SetSpriteTextureByIndex(m_sprite, m_currentFrame);
+		m_elapsedTime = 0.f;
+
+		m_currentFrame++;
+		if (m_currentFrame == end)
 		{
-			while (0)
-			{
-				for (int i = begin; i < end; i++)
-				{
-					m_sprite.setTexture(m_atlas.SetSpriteTextureByIndex);
-				}
-			}
+			if (m_loop)
+				m_currentFrame = m_states[m_currentstate].beg;
+			else
+				m_animIsPlaying = false;
 		}
-		else if(!m_loop)
-		{
-			for (int i = begin; i < end; i++)
-			{
-				m_sprite.setTexture(m_atlas.SetSpriteTextureByIndex);
-			}
-		}
-	}*/
+	}
 }
