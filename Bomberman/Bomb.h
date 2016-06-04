97a052f14f2420e087acd9ba85deb3d8fafdb64e
pietrzakacper <kacpietrzak@gmail.com>
2016-06-04 16:10:25 +0200

    Changing backslashes to slashes in SFML paths. Reason: My Linux Mint does not seem to recognize paths when backslahes are in use

--- Bomberman/Bomb.h
+++ Bomberman/Bomb.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <SFML\Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include <vector>
 #include "PhysicalBody.h"
 #include "Ray.h"
--- Bomberman/Player.h
+++ Bomberman/Player.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <SFML\Graphics.hpp>
+#include <SFML/Graphics.hpp>
 #include "PhysicalBody.h"
 #include "Bomb.h"
 
