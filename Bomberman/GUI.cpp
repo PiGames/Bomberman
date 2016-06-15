cc130cb860dbc63e7c07611f061c082f9b363b67
Condzi <koondzi12@wp.pl>
2016-06-15 15:14:36 +0200

    Yeah, I know it works :)! Thx Kacper

--- Bomberman/GUI.cpp
+++ Bomberman/GUI.cpp
@@ -6,23 +6,26 @@ void GUI::draw(sf::RenderTarget & target, sf::RenderStates states) const
 	std::vector<sf::Text> texts;
 	sf::Text tempText;
 	tempText.setFont(m_font);
+	tempText.setColor(sf::Color::Red);
 	tempText.setCharacterSize(m_textSize);
 	tempText.setPosition(0, 0);
 
 	for (short i = 0; i < m_respawns.size(); ++i)
 	{
 		// issue with displaying may cause this
-		tempText.setString(static_cast<char>(m_respawns[i]));
+		tempText.setString("Player " + std::to_string(i+1) + std::string(" respawns:") + std::to_string(m_respawns[i]));
 
 		if (i)
 		{
 			if (i == 1)
 			{
 				tempText.setPosition(tempText.getPosition().x + m_screenWidth - tempText.getGlobalBounds().width, tempText.getPosition().y);
+				tempText.setColor(sf::Color::Green);
 			}
 			else //if (i == 2)
 			{
 				tempText.setPosition(tempText.getPosition().x - m_screenWidth + tempText.getGlobalBounds().width, tempText.getPosition().y + m_screenHeight - tempText.getGlobalBounds().height);
+				tempText.setColor(sf::Color::Blue);
 			}
 		}
 
