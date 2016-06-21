f8089dfac758e5331183604e39dd60d5d789d657
pietrzakacper <kacpietrzak@gmail.com>
2016-06-21 17:13:46 +0200

    Changing appearance, I know I have better things to do but you cannot fight procrastination

--- Bomberman/GUI.cpp
+++ Bomberman/GUI.cpp
@@ -4,29 +4,27 @@
 void GUI::draw(sf::RenderTarget & target, sf::RenderStates states) const
 {
 	std::vector<sf::Text> texts;
-	sf::Text tempText;
-	tempText.setFont(*m_font);
-	tempText.setColor(sf::Color::Red);
-	tempText.setCharacterSize(m_textSize);
-	tempText.setPosition(0, 0);
+
 
 	for (short i = 0; i < m_respawns.size(); ++i)
 	{
+		sf::Text tempText;
+		tempText.setFont(*m_font);
+		tempText.setCharacterSize(m_textSize);
+		tempText.setPosition(0, 0);
 		tempText.setString("Player " + std::to_string(i+1) + std::string(" respawns:") + std::to_string(m_respawns[i]));
 
-		if (i)
+		if (i == 1)
 		{
-			if (i == 1)
-			{
-				tempText.setPosition(tempText.getPosition().x + m_screenWidth - tempText.getGlobalBounds().width, tempText.getPosition().y);
-				tempText.setColor(sf::Color::Green);
-			}
-			else //if (i == 2)
-			{
-				tempText.setPosition(tempText.getPosition().x - m_screenWidth + tempText.getGlobalBounds().width, tempText.getPosition().y + m_screenHeight - tempText.getGlobalBounds().height);
-				tempText.setColor(sf::Color::Blue);
-			}
+			tempText.setPosition(tempText.getPosition().x + m_screenWidth - tempText.getGlobalBounds().width-10, tempText.getPosition().y);
+			tempText.setColor(sf::Color::Green);
 		}
+		else 
+		{
+			tempText.setPosition(tempText.getPosition().x +10, tempText.getPosition().y);
+			tempText.setColor(sf::Color::Blue);
+		}
+		
 
 		texts.push_back(tempText);
 		target.draw(texts[i], states);
